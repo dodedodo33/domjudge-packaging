3ba24ce9400ac742314875ae12e86dbb4bcfe617
Dajo Hofman <d.hofman@dj33.nl>
2019-10-01 13:06:11 +0200

    Python 2.7 & mono added

--- docker/build.sh
+++ docker/build.sh
@@ -11,7 +11,7 @@ fi
 URL=https://www.domjudge.org/releases/domjudge-${VERSION}.tar.gz
 FILE=domjudge.tar.gz
 
-echo "[..] Downloading DOMJuge version ${VERSION}..."
+echo "[..] Downloading DOMJudge version ${VERSION}..."
 
 if ! curl -f -s -o ${FILE} ${URL}
 then
@@ -22,29 +22,29 @@ fi
 echo "[ok] DOMjudge version ${VERSION} downloaded as domjudge.tar.gz"; echo
 
 echo "[..] Building Docker image for domserver using intermediate build image..."
-docker build -t domjudge/domserver:${VERSION} -f domserver/Dockerfile .
+docker build -t registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/domserver:${VERSION} -f domserver/Dockerfile .
 echo "[ok] Done building Docker image for domserver"
 
 echo "[..] Building Docker image for judgehost using intermediate build image..."
-docker build -t domjudge/judgehost:${VERSION}-build -f judgehost/Dockerfile.build .
+docker build -t registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:${VERSION}-build -f judgehost/Dockerfile.build .
 docker rm -f domjudge-judgehost-${VERSION}-build > /dev/null 2>&1 || true
-docker run -it --name domjudge-judgehost-${VERSION}-build --privileged domjudge/judgehost:${VERSION}-build
+docker run -it --name domjudge-judgehost-${VERSION}-build --privileged registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:${VERSION}-build
 docker cp domjudge-judgehost-${VERSION}-build:/chroot.tar.gz .
 docker cp domjudge-judgehost-${VERSION}-build:/judgehost.tar.gz .
 docker rm -f domjudge-judgehost-${VERSION}-build
-docker rmi domjudge/judgehost:${VERSION}-build
-docker build -t domjudge/judgehost:${VERSION} -f judgehost/Dockerfile .
+docker rmi registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:${VERSION}-build
+docker build -t registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:${VERSION} -f judgehost/Dockerfile .
 echo "[ok] Done building Docker image for judgehost"
 
 echo "[..] Building Docker image for judgehost chroot..."
-docker build -t domjudge/default-judgehost-chroot:${VERSION} -f judgehost/Dockerfile.chroot .
+docker build -t registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/default-judgehost-chroot:${VERSION} -f judgehost/Dockerfile.chroot .
 echo "[ok] Done building Docker image for judgehost chroot"
 
-echo "All done. Image domjudge/domserver:${VERSION} and domjudge/judgehost:${VERSION} created"
+echo "All done. Image registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/domserver:${VERSION} and registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:${VERSION} created"
 echo "If you are a DOMjudge maintainer with access to the domjudge organization on Docker Hub, you can now run the following command to push them to Docker Hub:"
-echo "$ docker push domjudge/domserver:${VERSION} && docker push domjudge/judgehost:${VERSION} && docker push domjudge/default-judgehost-chroot:${VERSION}"
+echo "$ docker push registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/domserver:${VERSION} && docker push registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:${VERSION} && docker push registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/default-judgehost-chroot:${VERSION}"
 echo "If this is the latest release, also run the following command:"
-echo "$ docker tag domjudge/domserver:${VERSION} domjudge/domserver:latest && \
-docker tag domjudge/judgehost:${VERSION} domjudge/judgehost:latest && \
-docker tag domjudge/default-judgehost-chroot:${VERSION} domjudge/default-judgehost-chroot:latest && \
-docker push domjudge/domserver:latest && docker push domjudge/judgehost:latest && docker push domjudge/default-judgehost-chroot:latest"
+echo "$ docker tag registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/domserver:${VERSION} registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/domserver:latest && \
+docker tag registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:${VERSION} registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:latest && \
+docker tag registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/default-judgehost-chroot:${VERSION} registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/default-judgehost-chroot:latest && \
+docker push registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/domserver:latest && docker push registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/judgehost:latest && docker push registry.dj33.nl/error418/hbo-i_apc/domjudge-packaging/default-judgehost-chroot:latest"
